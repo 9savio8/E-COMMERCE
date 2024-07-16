98705b567fd68d5589307080214dfa03a0f8fb97
macchia <143026004+Macchiavellii@users.noreply.github.com>
2024-07-16 16:13:37 +0200

    git forgor

---
+++ controllers/userController.js
@@ -0,0 +1,36 @@
+// Importa il modello User
+const User = require('./userController');
+
+// Simulazione di un database
+const users = [];
+
+// Funzioni per interagire con il "database"
+const addUser = (id,email,name,surname, dateOfBird, address, hashedPassword) => {
+    const existingUserById = findUserById(id);
+    if (existingUserById) {
+        throw new Error('User with this ID already exists');
+}
+
+// Creazione di un nuovo utente utilizzando il modello User
+const newUser = new User(id,email,name,surname,dateOfBirth,address,hashedPassword);
+users.push(newUser);
+
+return newUser;
+};
+
+const findUserByName = (name) => {
+    return users.find(user => user.email === email);
+};
+
+
+// Funzione per trovare un utente per id
+const findUserById = (id) => {
+    return users.find(user => user.id === id);
+};
+
+module.exports = {
+    addUser,
+    findUserByName,
+    findUserById
+};
+
