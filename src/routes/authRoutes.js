231c96003420fb0b77a61f90db7d89bd0516c9f3
savio_imbrosciano <savioimbrosciano32@gmail.com>
2024-07-17 16:27:23 +0200

    token fix

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,10 +1,12 @@
 const express = require('express');
-const { register, login, getUserById } = require('../controllers/authController');
+const { register, login, getUserById, userLogOut } = require('../controllers/authController');
 
 const router = express.Router();
 
 router.post('/register', register);
 router.post('/login', login);
+router.get('/logout', userLogOut)
+
 router.get('/:id', getUserById);
 
 module.exports = router;
