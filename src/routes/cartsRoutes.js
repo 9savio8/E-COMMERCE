c4e6f1a77dec36ec2aa48b224cd0721497a1e172
savio_imbrosciano <savioimbrosciano32@gmail.com>
2024-07-18 16:09:29 +0200

    cartsController fix

--- src/routes/cartsRoutes.js
+++ src/routes/cartsRoutes.js
@@ -1,17 +1,18 @@
 const express = require('express');
 const router = express.Router();
 const cartController = require('../controllers/cartsController');
+const verifyToken = require('../middleware/authentication');  
 
 // Rotta per ottenere il contenuto del carrello
 router.get('/', cartController.getCart);
 
 // Rotta per aggiungere un prodotto al carrello
-router.post('/add', cartController.addToCart);
+router.post('/add', verifyToken, cartController.addToCart);
 
 // Rotta per rimuovere un prodotto dal carrello
 router.delete('/remove/:id', cartController.removeFromCart);
 
 // Rotta per svuotare il carrello
-router.delete('/clear', cartController.clearCart);
+router.delete('/clear',verifyToken, cartController.clearCart);
 
 module.exports = router;
